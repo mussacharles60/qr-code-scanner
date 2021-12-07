c40de957cb5fd2a4a62cabf118ccd7ed48c9a186
Mussa Charles <mussacharles60@gmail.com>
2021-12-07 18:10:31 +0300

    update user interface

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,5 +1,6 @@
-var button = document.getElementById('quiz-button');
+var button = document.getElementById('btn');
 button.onclick = () => {
-    window.open("https://quiz-score-crdb.herokuapp.com/quiz", "_self")
+    //window.open("https://quiz-score-crdb.herokuapp.com/quiz", "_self")
     // window.open("http://localhost:8000/quiz", "_self")
+    window.location = "/scanner";
 }
